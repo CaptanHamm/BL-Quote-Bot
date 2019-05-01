0f646fc90f1bcdd4449ea5a82c909120257788d3
Captain Hammer <36648742+CaptanHamm@users.noreply.github.com>
2019-05-01 08:32:53 -0500

    adding eslintrc file

--- commands/help.js
+++ commands/help.js
@@ -1,30 +1,24 @@
-module.exports = async ( client, message ) => {
-
-	try {
-
-		let embed = {};
-
-		embed.title = client.user.username+" help";
-
-		embed.description = '`------------------------------`\n';
-		embed.description += "This is help...\n";
-		embed.description += '`------------------------------`\n';
-		embed.description += '`Help --------- help, h`\n'
-		embed.description += '`Displays this message`\n';
-		embed.description += '`Quote --------- q quote`\n';
-		embed.description += '`Sends a quote from Borderlands`\n';
-		embed.description += '`sub commands include jack/j psycho/p torgue/t`\n';
-		embed.description += '`Support --------- support`\n'
-		embed.description += '`Displays the support links`\n';
-
-		embed.color = 0x2A6EBB;
-		embed.timestamp = new Date();
-
-		message.react('ℹ');
-		message.channel.send({embed});
-
-	} catch(e) {
-		throw e;
-	}
-
-}
+module.exports = async (client, message) => {
+  try {
+    let embed = {};
+    embed.title = client.user.username + " help";
+    embed.description = "`------------------------------`\n";
+    embed.description += "This is help...\n";
+    embed.description += "`------------------------------`\n";
+    embed.description += "`Help --------- help, h`\n";
+    embed.description += "`Displays this message`\n";
+    embed.description += "`Quote --------- q quote`\n";
+    embed.description += "`Sends a quote from Borderlands`\n";
+    embed.description += "`sub commands include jack/j psycho/p torgue/t`\n";
+    embed.description += "`Support --------- support`\n";
+    embed.description += "`Displays the support links`\n";
+    embed.color = 0x2a6ebb;
+    embed.timestamp = new Date();
+    message.react("ℹ");
+    message.channel.send({
+      embed
+    });
+  } catch (e) {
+    throw e;
+  }
+};
